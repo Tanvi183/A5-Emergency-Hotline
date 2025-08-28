c833d1fc1cfabc2a8fc2042748ab7b6e12ee0397
tanvi183 <mneshat7@gmail.com>
2025-08-28 11:49:05 -0700

    increase the count

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let lifeLine = 0;
 
+const lifeLineStore = document.getElementById("lifeLineStore");
+const buttons = document.querySelectorAll(".increment-btn");
+
+for(const btn of buttons){
+    btn.addEventListener("click", function(){
+        lifeLine++;
+        lifeLineStore.innerText = lifeLine;
+    })    
+}
